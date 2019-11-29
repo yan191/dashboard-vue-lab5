84771a37d8eabda74280ff923377105986c37b7b
Yan Rodrigues <yan.azevedo@gsw.com.br>
2019-11-28 21:17:50 -0300

    Formulários de cadastro

--- src/components/ApontamentosForm.vue
+++ src/components/ApontamentosForm.vue
@@ -1,15 +1,108 @@
 <template>
     <div class="apontamentos-form">
-        Apontamentos form
+        <v-card class="form">
+            <v-card-title primary-title>
+                FAZER APONTAMENTO
+            </v-card-title>
+
+            <v-card-text>
+                <v-form ref="apontamentosForm">
+                    <v-select :items="projetos"
+                            v-model="projeto"
+                            :rules="projeto_rules"
+                            item-value="id"
+                            item-text="nome"
+                            label="Projetos"
+                            outlined>
+                    </v-select>
+                    <v-text-field label="Horas trabalhadas"
+                                v-model="horas"
+                                :rules="horas_rules"
+                                type="number"
+                                outlined>
+                    </v-text-field>
+                </v-form>
+                <v-btn @click="apontar" :disabled="disableBtn" depressed dark color="#621055">apontar</v-btn>
+            </v-card-text>
+        </v-card>
+        <v-snackbar v-model="snackbar"
+                    color="#b52b65"
+                    :timeout="3000"
+                    :top="true">
+                    {{ snackbarTxt }}
+                    <v-btn dark
+                            text
+                            @click="snackbar = false">
+                            Fechar
+                    </v-btn>
+        </v-snackbar>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'apontamentos-form'
+    name: 'apontamentos-form',
+    data: () => ({
+        projetos: [],
+        projeto: null,
+        valid: false,
+        horas: 0,
+        projeto_rules: [
+            projeto => !!projeto || 'Campo obrigatório',
+            projeto => projeto > 0 || 'Campo inválido'
+        ],
+        horas_rules: [
+            horas => !!horas || 'Campo obrigatório',
+            horas => horas > 0 || 'Deve ser maior que 0'
+        ],
+        snackbarTxt: '',
+        snackbar: false,
+        disableBtn: true
+    }),
+    mounted () {
+        const app = this;
+        this.axios.get('https://lab5-fatec.herokuapp.com/projeto')
+        .then(function (response) {
+            app.projetos = response.data;
+            app.disableBtn = false;
+        });
+    },
+    methods: {
+        apontar() {
+            if (this.$refs.apontamentosForm.validate()) {
+                const projeto_id = this.projeto;
+                const horas = this.horas;
+                const app = this;
+                this.disableBtn = true;
+
+                this.axios.post('https://lab5-fatec.herokuapp.com/apontamento', {horas, projeto_id})
+                .then(function() {
+                    app.snackbarTxt = 'Projeto cadastrado com sucesso';
+                    app.snackbar = true;
+                    app.projeto = null;
+                    app.horas = 0;
+                    app.disableBtn = false;
+                }).catch(function (){
+                    app.snackbarTxt = 'Erro no sistema, aguarde e tente novamente';
+                    app.snackbar = true;
+                    app.disableBtn = false;
+                });
+            }
+        }
+    }
 }
 </script>
 
 <style scoped>
+    .apontamentos-form {
+        width: 100vw;
+        display: flex;
+        flex-direction: column;
+    }
 
+    .form {
+        width: 600px;
+        max-width: 90vw;
+        margin: auto;
+    }
 </style>
--- src/components/ProjetosForm.vue
+++ src/components/ProjetosForm.vue
@@ -6,16 +6,34 @@
             </v-card-title>
 
             <v-card-text>
-                <v-form ref="projetoForm">
+                <v-form ref="projetoForm" v-model="valid">
                     <v-text-field label="Nome"
+                                v-model="nome"
+                                :rules="nome_rules"
                                 outlined>
                     </v-text-field>
                     <v-text-field label="Horas planejadas"
+                                v-model="horas_planejadas"
+                                :rules="horas_rules"
+                                type="number"
                                 outlined>
                     </v-text-field>
                 </v-form>
+
+                <v-btn @click="criarProjeto" :disabled="disableBtn" depressed dark color="#621055">cadastrar</v-btn>
             </v-card-text>
         </v-card>
+        <v-snackbar v-model="snackbar"
+                    color="#b52b65"
+                    :timeout="3000"
+                    :top="true">
+                    {{ snackbarTxt }}
+                    <v-btn dark
+                            text
+                            @click="snackbar = false">
+                            Fechar
+                    </v-btn>
+        </v-snackbar>
     </div>
 </template>
 
@@ -28,13 +46,39 @@ export default {
         horas_planejadas: 0,
         nome_rules: [
             nome => !!nome || 'Campo obrigatório',
-            nome => nome.length < 4 || 'Min. 4 caracteres'
+            nome => nome.length >= 4 || 'Min. 4 caracteres'
         ],
         horas_rules: [
             horas => !!horas || 'Campo obrigatório',
-            horas => horas <= 0 || 'Deve ser maior que 0'
-        ]
-    })
+            horas => horas > 0 || 'Deve ser maior que 0'
+        ],
+        snackbarTxt: '',
+        snackbar: false,
+        disableBtn: false
+    }),
+    methods: {
+        criarProjeto () {
+            if (this.$refs.projetoForm.validate()) {
+                const nome = this.nome;
+                const horas_planejadas = this.horas_planejadas;
+                const app = this;
+                this.disableBtn = true;
+    
+                this.axios.post('https://lab5-fatec.herokuapp.com/projeto', {nome, horas_planejadas})
+                .then(function() {
+                    app.snackbarTxt = 'Projeto cadastrado com sucesso';
+                    app.snackbar = true;
+                    app.nome = '';
+                    app.horas_planejadas = 0;
+                    app.disableBtn = false;
+                }).catch(function (){
+                    app.snackbarTxt = 'Erro no sistema, aguarde e tente novamente';
+                    app.snackbar = true;
+                    app.disableBtn = false;
+                });
+            }
+        }
+    }
 }
 </script>
 
@@ -50,5 +94,4 @@ export default {
         max-width: 90vw;
         margin: auto;
     }
-
 </style>
